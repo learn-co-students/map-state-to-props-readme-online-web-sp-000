6dc41a1401a80021c93d68df129396c7164f2223
github username <github email address>
2019-10-30 14:36:45 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -4,6 +4,9 @@ import { createStore } from 'redux';
 import shoppingListItemReducer from './reducers/shoppingListItemReducer';
 import App from './App';
 import './index.css';
+import { Provider } from 'react-redux'; /* code change */
+//just imported Provide form React Redux
+
 
 const store = createStore(
   shoppingListItemReducer,
@@ -11,6 +14,10 @@ const store = createStore(
 );
 
 ReactDOM.render(
-  <App store={store}/>,
+  <Provider store={store}/>, //we passed our store instance into Provider as a prop which makes it available to all of the other components
+    <App />
+ </Provider>, /* code change */
+ //we used Provider to wrap our React app
   document.getElementById('root')
+
 );
