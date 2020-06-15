6e608c3ec9358125ce201afdc13ff91443f4f196
Amy Westcott <amy.westcott@yahoo.com>
2020-06-15 16:06:32 -0600

    Done.

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { createStore } from 'redux';
+import { Provider } from 'react-redux';
 import shoppingListItemReducer from './reducers/shoppingListItemReducer';
 import App from './App';
 import './index.css';
